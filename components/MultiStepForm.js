22538d8386ba7e65ec0f2018dfd31f13deabbf29
carlosguso <cargusooriginal@gmail.com>
2020-06-09 19:06:09 -0500

    Error displaying bar,list and input. Subject and name validation

--- components/MultiStepForm.js
+++ components/MultiStepForm.js
@@ -3,24 +3,33 @@ import React, {useEffect,useState} from 'react'
 
 export default function MultiStepForm() {
     //State 0: Enter subject.
-    //State 1: Enter email.
-    //State 2: enter name.
+    //State 1: Enter name.
+    //State 2: Enter email.
     //State 3: Enter message.
     //State 4: Completed. 
     const [progress, setprogress] = useState(0);
     const [form, setform] = useState({
         subject: "",
-        email: "",
         name: "",
+        email: "",
         message: ""
     });
+    const [errors, seterrors] = useState([]);
+
+    useEffect(() => {
+        //console.log(progress);
+    }, [progress])
+
+    useEffect(() => {
+        //console.log(form);
+    }, [form])
 
     const drawBar = () => {
         let bar = [];
         for(let i = 0; i < 4; i++) {
             //Render progress dot
             if(i <= progress) {
-                bar.push(<li key={i} className="active"></li>);
+                bar.push(<li key={i} className={errors.length != 0 && i == progress ? "error" : "active"}></li>);
             } else {
                 bar.push(<li key={i}></li>);
             }
@@ -34,36 +43,64 @@ export default function MultiStepForm() {
         return bar;
     }
 
-    useEffect(() => {
-        console.log(progress);
-    }, [progress])
-
-    useEffect(() => {
-        console.log(form);
-    }, [form])
-
     const handleStep = (e,step) => {
         e.preventDefault();
+        const resErr = evalFormData()
         if(step > 0 ) {
-            setprogress(progress < 4 ? progress+1 : 4);
+            if(resErr === true) {
+                setprogress(progress < 4 ? progress+1 : 4);
+                seterrors([]);
+            } else {
+                seterrors(resErr);
+            }
         } else {
             setprogress(progress > 0 ? progress-1 : 0);
+            seterrors([]);
         }
-        handleFormData();
+        console.log(form);
     }
 
-    const handleFormData = () => {
-        //console.log("formadata progress: " +  progress);
+    const handleFormData = (e) => {
+        setform({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    } 
+
+    const evalFormData = () => {
+        let errors = [];
         switch(progress) {
             case 0:
-                const val = document.getElementsByTagName("input")[0].value;
-                setform({...form,subject: val});
+                if(form.subject.length != 0){
+                    return true;
+                } else{
+                    errors.push("Subject can´t be empty");
+                }
+                break;
+
+            case 1: 
+                if(form.name.length != 0){
+                    return true;
+                } else{
+                    errors.push("Subject can´t be empty");
+                }
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+                break;
+
+            case 4:
                 break;
 
             default:
                 break;
         }
-    } 
+        return errors;
+    }
 
     return (
         <form className="form">
@@ -73,27 +110,34 @@ export default function MultiStepForm() {
             {progress == 0 &&
                 <div className="form-input">
                     <span>Subject:</span>
-                    <input type="text" placeholder="Subject of the matter"></input>
+                    <input value={form.subject} className={errors.length != 0 ? "error" : ""} type="text" onChange={handleFormData} placeholder="Subject of the matter" name="subject"></input>
                 </div>
             }
             {progress == 1 &&
                 <div className="form-input">
-                    <span>Email:</span>
-                    <input type="text" placeholder="john@example.com"></input>
+                    <span>Name:</span>
+                    <input value={form.name} className={errors.length != 0 ? "error" : ""} type="text" onChange={handleFormData} placeholder="John doe" name="name"></input>
                 </div>
             }
             {progress == 2 &&
                 <div className="form-input">
-                    <span>Name:</span>
-                    <input type="text" placeholder="John doe"></input>
+                    <span>Email:</span>
+                    <input value={form.email} className={errors.length != 0 ? "error" : ""} type="text" onChange={handleFormData} placeholder="john@example.com" name="email"></input>
                 </div>
             }
             {progress == 3 &&
                 <div className="form-input">
                     <span>Message:</span>
                     <br/>
-                    <textarea placeholder=" Message..."></textarea>
+                    <textarea value={form.message} className={errors.length != 0 ? "error" : ""} placeholder=" Message..." onChange={handleFormData} name="message"></textarea>
                 </div>
+            }
+            {errors.length != 0 &&
+                <ul className="form-errors">
+                    {errors.map((error,i) => {
+                        return <li key={"error"+i}>{error}</li>
+                    })}
+                </ul>
             }
             <div className="form-button-wrapper">
                 <button className={progress == 0 ? "disabled": ""} onClick={e => {handleStep(e,-1)}}>Back</button>
